e72180cc68c0cf3f18e6148db47a4b497d4396ef
Max Zilberman <mazilber@microsoft.com>
2020-04-08 18:39:38 -0400

    Adding basic test for Register

---
+++ ServiceTests/TestRegister.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using CatanSharedModels;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Xunit;
+
+namespace ServiceTests
+{
+    public class TestRegister: IClassFixture<ServiceFixture>
+    {
+        private readonly ServiceFixture _fixture;
+
+        public TestRegister(ServiceFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        async Task CatanRegisterReturnsSuccess()
+        {
+            string url = $"/api/catan/game/register/{_fixture.GameName}/Max";
+            var response = await _fixture.HttpClient.PostAsync(url, new StringContent(CatanSerializer.Serialize<GameInfo>(new GameInfo()), Encoding.UTF8, "application/json"));
+            string body = await response.Content.ReadAsStringAsync();
+            
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+    }
+}
