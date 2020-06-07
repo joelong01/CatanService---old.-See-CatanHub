a18ac982083a8f1cb1e53a6ebad1480ac99b45f0
Joe Long <joelong@outlook.com>
2020-06-07 16:50:42 -0700

    added support for SignalR groups

--- CatanService/SignalRHubs/CatanHub.cs
+++ CatanService/SignalRHubs/CatanHub.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Catan.Proxy;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
@@ -8,9 +9,29 @@ namespace CatanService
 
     public class CatanHub : Hub
     {
-        public void BroadcastMessage(string name, string message)
+        public Task BroadcastMessage(string gameName, string playerName, string message)
         {
-            Clients.All.SendAsync("BroadcastMessage", name, message);
+            return Clients.Group(gameName).SendAsync("BroadcastMessage", playerName, message);
+        }
+
+        public async Task JoinGame(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            
+            await Clients.Group(groupName).SendAsync("JoinedGame", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        public async Task LeaveGame(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("LeftGame", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
+
+        public Task SendPrivateMessage(string playerName, string message)
+        {
+            return Clients.User(playerName).SendAsync("PrivateMessage", message);
         }
     }
 
